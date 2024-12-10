40ede0bc0c2c76630cefa44ddee2c1edd61ce26d
thorsten albers <talbers1.ta@gmail.com>
2024-12-10 11:23:18 +0100

    Initial Commit

---
+++ TToolsIPScanner/TToolsIPScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TToolsIPScannerApp.swift
+//  TToolsIPScanner
+//
+//  Created by Thorsten Albers on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TToolsIPScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TToolsIPScannerTests/TToolsIPScannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TToolsIPScannerTests.swift
+//  TToolsIPScannerTests
+//
+//  Created by Thorsten Albers on 10.12.24.
+//
+
+import Testing
+
+struct TToolsIPScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
